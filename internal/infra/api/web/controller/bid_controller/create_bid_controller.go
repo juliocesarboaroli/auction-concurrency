61715ca0098aefd097b4f85496c523874643661b
Julio César Mello Boaroli <juliocesarboaroli@gmail.com>
2026-06-27 00:10:02 -0300

    feat: base project

---
+++ internal/infra/api/web/controller/bid_controller/create_bid_controller.go
@@ -0,0 +1,43 @@
+package bid_controller
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/juliocesarboaroli/auction-concurrency/configuration/rest_err"
+	"github.com/juliocesarboaroli/auction-concurrency/internal/infra/api/web/validation"
+	"github.com/juliocesarboaroli/auction-concurrency/internal/usecase/bid_usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BidController struct {
+	bidUseCase bid_usecase.BidUseCaseInterface
+}
+
+func NewBidController(bidUseCase bid_usecase.BidUseCaseInterface) *BidController {
+	return &BidController{
+		bidUseCase: bidUseCase,
+	}
+}
+
+func (u *BidController) CreateBid(c *gin.Context) {
+	var bidInputDTO bid_usecase.BidInputDTO
+
+	if err := c.ShouldBindJSON(&bidInputDTO); err != nil {
+		restErr := validation.ValidateErr(err)
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	err := u.bidUseCase.CreateBid(context.Background(), bidInputDTO)
+	if err != nil {
+		restErr := rest_err.ConvertError(err)
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	c.Status(http.StatusCreated)
+}
